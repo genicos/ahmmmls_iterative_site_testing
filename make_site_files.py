f28b5ace9d3d702b481644a07585d38da7d41798
Nico Ayala <nmayala@ucsc.edu>
2023-02-14 19:43:16 +0000

    A lot of extra stuff, pushing just in case

--- make_site_files.py
+++ make_site_files.py
@@ -3,27 +3,13 @@ import sys
 site_index = int(sys.argv[1])
 
 
-sites_to_test = [
-0.066181,
-0.111915,
-0.275800,
-0.287784,
-0.135391,
-0.324536,
-0.030964,
-0.050068,
-0.152171,
-0.375822,
-0.416547,
-0.173733,
-]
-
+site_to_test = float(open("sites_to_test").readlines()[site_index])
 
 bound_radius = 0.005
 
 
 best_site_file = open("best_site_file","r").read()[:-1]
-addendum = " l (" + str(sites_to_test[site_index]) + ") " + str(sites_to_test[site_index] - bound_radius) + " " + str(sites_to_test[site_index] + bound_radius) + " h 0.5 s ()+ "
+addendum = " l (" + str(site_to_test) + ") " + str(site_to_test - bound_radius) + " " + str(site_to_test + bound_radius) + " h 0.5 s ()+ "
 
 
 sim_sitefile = open("3r_simulation/site_file","w")
