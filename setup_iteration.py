a23c240e732d8ee8ae354047f8e4053894164dff
Nicolas Ayala <nicoatnico.com@gmail.com>
2023-03-09 12:57:38 -0800

    using demographics to create config file

--- setup_iteration.py
+++ setup_iteration.py
@@ -1,5 +1,6 @@
 import sys
 
+demo = open("chrom_and_demo/demographics").readlines()
 
 best_lnl = open("best_lnl","w")
 best_lnl.write("-999999999999")
@@ -7,4 +8,4 @@ best_lnl.write("-999999999999")
 best_model = open("best_model", "w")
 
 best_model = open("best_site_file", "w")
-best_model.write("0 test n 0.138252941724984 466 ")
+best_model.write("0 test n "+m+" "+t+" ")
